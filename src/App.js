9d80f1a99f39db0197364f4be0c0a0470bedab2c
Jonah Lewis <jonahslewis96@gmail.com>
2023-06-10 20:54:26 -0700

    added react-router-dom dependency

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header'
 import About from './components/About';
 import Resume from './components/Resume';
